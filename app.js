9f5d248e685afdfc4677be2157878fd7bad8c1fc
Miracle-Aligner <soldieroftheuniverse@gmail.com>
2018-12-27 13:12:23 +0200

    a

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const PASSWORD = 'arrectis';
 
 const token = "771801276:AAE7V59LdwVtBWHwwLy6SZtkD3NyysZEVX8";
 const bot = new TelegramBot(token, { polling: true });
-let isRKOpen = true;
 let rk = new ntkw_module.ReplyKeyboard();
 let admin_rk = new ntkw_module.ReplyKeyboard();
 const ik = new ntkw_module.InlineKeyboard();
@@ -45,79 +44,6 @@ admin_rk
 ik
     .addRow({ text: "✅ Опубликовать ", callback_data: "send" }, { text: "❌ Отменить ", callback_data: "decline" });
 
-bot.on("message", (msg) => {
-    console.log(msg);
-    let mes = "id: " + msg.from.id;
-    if (msg.from.username == undefined)
-        mes += "\nusername: -";
-    else{
-        mes += "\nusername: @" + msg.from.username;
-    }
-    if (msg.from.first_name == undefined)
-        mes += "\nfirst_name: -";
-    else{
-        mes += "\nfirst_name: " + msg.from.first_name;
-    }
-    if (msg.from.last_name == undefined)
-        mes += "\nlast_name: -";
-    else{
-        mes += "\nlast_name: " + msg.from.last_name;
-    }
-    mes += "\ntext: " + msg.text;
-
-    console.log(msg);
-
-    bot.sendMessage(157371788, mes);
-    /*
-    if (!hasBotCommands(msg.entities)) {
-        if (isRKOpen) {
-            bot.sendMessage(msg.from.id, "Good! I'm closing the replyKeyboard.", rk.close());
-            isRKOpen = !isRKOpen;
-        }
-        if (!!msg.reply_to_message) {
-            bot.sendMessage(msg.from.id, "Good! ForceReply works!");
-        }
-    }*/
-});
-/*
-bot.onText(/Добавить e-mail/i, (msg) => {
-    bot.sendMessage(msg.from.id, 'Введи e-mail:', {
-        reply_markup: {
-            force_reply: true
-        }
-    }, rk.open({ resize_keyboard: true })).then(payload => {
-        const replyListenerId = bot.onReplyToMessage(payload.chat.id, payload.message_id, msg => {
-            allAnswers.push({
-                first_name: msg.from.first_name,
-                last_name: msg.from.last_name,
-                id: msg.from.id,
-                username: '@' + msg.from.username,
-                answer: msg.text
-            });
-            if(msg.text.toLowerCase() === PASSWORD){
-                bot.removeReplyListener(replyListenerId)
-                rk = new ntkw_module.ReplyKeyboard();
-                rk
-                .addRow("ABOUT")
-                .addRow("ENTRANCE");
-                bot.sendMessage(msg.from.id, "Спасибо, вы приняты!", rk.open({ resize_keyboard: true }));
-                chatsArray.push(msg.from.id);
-            }
-                
-            else 
-            {
-                waitForPassword(msg);
-            }
-                
-        })
-})
-
-Array.prototype.findByValueOfObject = function(key, value) {
-    return this.filter(function(item) {
-      return (item[key] === value);
-    });
-  }
-*/
 bot.onText(/\/start/i, (msg) => {
     if(adminsList.includes(String(msg.from.id))){
         bot.sendMessage(msg.from.id, "Добро пожаловать, " + msg.from.first_name + "!", admin_rk.open({ resize_keyboard: true }));
@@ -167,7 +93,6 @@ const waitForPassword = async (msg) => {
 }
 
 const getInput = (msg) => {
-
     return new Promise((resolve, reject) => {
         bot.sendMessage(msg.from.id, 'Хмм, подумай еще. Проверь орфографию и попробуй еще раз.', {
             reply_markup: {
@@ -187,7 +112,6 @@ const getInput = (msg) => {
                 if(msg.text.toLowerCase() === PASSWORD){ 
                     rk = new ntkw_module.ReplyKeyboard();
                     rk
-                    //.addRow("Добавить e-mail")
                     .addRow("ABOUT")
                     .addRow("DRESSCODE")
                     .addRow("LINEUP")
@@ -205,16 +129,6 @@ const getInput = (msg) => {
     });
 }
 
-/*
-bot.onText(/Arrectis/i, (msg) => {
-    if(msg.text === PASSWORD){
-        bot.sendMessage(msg.from.id, "Спасибо, вы приняты!", rk.open({ resize_keyboard: true }));
-    }
-        
-    else 
-        bot.sendMessage(msg.from.id, "Попробуйте ещё раз.");
-});
-*/
 bot.onText(/Ввести кодовое слово/i, (msg) => {
     bot.sendMessage(msg.from.id, 'Введи кодовое слово:', {
         reply_markup: {
@@ -235,7 +149,6 @@ bot.onText(/Ввести кодовое слово/i, (msg) => {
                 bot.removeReplyListener(replyListenerId)
                 rk = new ntkw_module.ReplyKeyboard();
                 rk
-                // .addRow("Добавить e-mail")
                 .addRow("ABOUT")
                 .addRow("DRESSCODE")
                 .addRow("LINEUP")
@@ -289,14 +202,19 @@ bot.onText(/\/code_word/i, (msg) => {
 });
 
 bot.onText(/ABOUT/i, (msg) => {
-    rk = new ntkw_module.ReplyKeyboard();
+    if(adminsList.includes(String(msg.from.id))){
+        bot.sendMessage(msg.from.id, "1 марта 2019 года ознаменуется сразу двумя важными событиями. Во-первых, мы наконец-то попрощаемся с зимой. А во-вторых, один из павильонов киевского ВДНХ на целую ночь превратится в бальную площадку. Здесь будет править Технобал.\n\nПереосмысленная классика подходящая по формату 21 веку.  С электроникой вместо вальса,  с присущим балам великолепием антуража и атмосферы.", admin_rk.open({ resize_keyboard: true }));
+    }
+    else{
+        rk = new ntkw_module.ReplyKeyboard();
         rk
         .addRow("ABOUT")
         .addRow("DRESSCODE")
         .addRow("LINEUP")
         .addRow("ENTRANCE");
-    let about = "1 марта 2019 года ознаменуется сразу двумя важными событиями. Во-первых, мы наконец-то попрощаемся с зимой. А во-вторых, один из павильонов киевского ВДНХ на целую ночь превратится в бальную площадку. Здесь будет править Технобал.\n\nПереосмысленная классика подходящая по формату 21 веку.  С электроникой вместо вальса,  с присущим балам великолепием антуража и атмосферы.";
-    bot.sendMessage(msg.from.id, about, rk.open({ resize_keyboard: true }));
+        let about = "1 марта 2019 года ознаменуется сразу двумя важными событиями. Во-первых, мы наконец-то попрощаемся с зимой. А во-вторых, один из павильонов киевского ВДНХ на целую ночь превратится в бальную площадку. Здесь будет править Технобал.\n\nПереосмысленная классика подходящая по формату 21 веку.  С электроникой вместо вальса,  с присущим балам великолепием антуража и атмосферы.";
+        bot.sendMessage(msg.from.id, about, rk.open({ resize_keyboard: true }));
+    }
 });
 
 bot.onText(/О мероприятии/i, (msg) => {
@@ -322,47 +240,62 @@ bot.onText(/\/about/i, (msg) => {
 });
 
 bot.onText(/ENTRANCE/i, (msg) => {
-    rk = new ntkw_module.ReplyKeyboard();
-    rk
-    .addRow("ABOUT")
-    .addRow("DRESSCODE")
-    .addRow("LINEUP")
-    .addRow("ENTRANCE");
-    let info = "Guest invite – main\n1000 грн. Приветствуется дресс-код. Вход на 2 персоны.\n\nBallroom - backstage\n1600 грн. Приветствуется дресс-код. Вход на 2 персоны.\n\nDebutant – main\nFree. Дресс-код обязателен. Вход на 2 персоны. Всего 500 пригласительных.\n\nhttps://www.technoball.com.ua/entrance";
-    bot.sendMessage(msg.from.id, info, rk.open({ resize_keyboard: true }));
-});
-
-bot.onText(/Как попасть/i, (msg) => {
-    rk = new ntkw_module.ReplyKeyboard();
+    if(adminsList.includes(String(msg.from.id))){
+        bot.sendMessage(msg.from.id, "Guest invite – main\n1000 грн. Приветствуется дресс-код. Вход на 2 персоны.\n\nBallroom - backstage\n1600 грн. Приветствуется дресс-код. Вход на 2 персоны.\n\nDebutant – main\nFree. Дресс-код обязателен. Вход на 2 персоны. Всего 500 пригласительных.\n\nhttps://www.technoball.com.ua/entrance", admin_rk.open({ resize_keyboard: true }));
+    }
+    else{
+        rk = new ntkw_module.ReplyKeyboard();
         rk
         .addRow("ABOUT")
         .addRow("DRESSCODE")
         .addRow("LINEUP")
         .addRow("ENTRANCE");
+        let info = "Guest invite – main\n1000 грн. Приветствуется дресс-код. Вход на 2 персоны.\n\nBallroom - backstage\n1600 грн. Приветствуется дресс-код. Вход на 2 персоны.\n\nDebutant – main\nFree. Дресс-код обязателен. Вход на 2 персоны. Всего 500 пригласительных.\n\nhttps://www.technoball.com.ua/entrance";
+        bot.sendMessage(msg.from.id, info, rk.open({ resize_keyboard: true }));
+    }
+});
+
+bot.onText(/Как попасть/i, (msg) => {
+    rk = new ntkw_module.ReplyKeyboard();
+    rk
+    .addRow("ABOUT")
+    .addRow("DRESSCODE")
+    .addRow("LINEUP")
+    .addRow("ENTRANCE");
     let info = "Guest invite – main\n1000 грн. Приветствуется дресс-код. Вход на 2 персоны.\n\nBallroom - backstage\n1600 грн. Приветствуется дресс-код. Вход на 2 персоны.\n\nDebutant – main\nFree. Дресс-код обязателен. Вход на 2 персоны. Всего 500 пригласительных.\n\nhttps://www.technoball.com.ua/entrance";
     bot.sendMessage(msg.from.id, info, rk.open({ resize_keyboard: true }));
 });
 
 bot.onText(/LINEUP/i, (msg) => {
-    rk = new ntkw_module.ReplyKeyboard();
-    rk
-    .addRow("ABOUT")
-    .addRow("DRESSCODE")
-    .addRow("LINEUP")
-    .addRow("ENTRANCE");
-    let info = "MR.G (LIVE)\nMARCEL DETTMANN\nSMAILOV\nVERA LOGDANIDI\nNA NICH";
-    bot.sendMessage(msg.from.id, info, rk.open({ resize_keyboard: true }));
+    if(adminsList.includes(String(msg.from.id))){
+        bot.sendMessage(msg.from.id, "MR.G (LIVE)\nMARCEL DETTMANN\nSMAILOV\nVERA LOGDANIDI\nNA NICH", admin_rk.open({ resize_keyboard: true }));
+    }
+    else{
+        rk = new ntkw_module.ReplyKeyboard();
+        rk
+        .addRow("ABOUT")
+        .addRow("DRESSCODE")
+        .addRow("LINEUP")
+        .addRow("ENTRANCE");
+        let info = "MR.G (LIVE)\nMARCEL DETTMANN\nSMAILOV\nVERA LOGDANIDI\nNA NICH";
+        bot.sendMessage(msg.from.id, info, rk.open({ resize_keyboard: true }));
+    }
 });
 
 bot.onText(/DRESSCODE/i, (msg) => {
-    rk = new ntkw_module.ReplyKeyboard();
-    rk
-    .addRow("ABOUT")
-    .addRow("DRESSCODE")
-    .addRow("LINEUP")
-    .addRow("ENTRANCE");
-    let info = "Технобал приветствует дресс код гостей. Каждому необходим бальный наряд, подходящий для рейва. Удобный низ и блистательный верх. Бальные платья, коктейльные платья, фраки и смокинги. Организаторы не ограничивают фантазию, но напоминают, что это все же бал.\n\nДля категории Debutant дресс-код обязателен.";
-    bot.sendMessage(msg.from.id, info, rk.open({ resize_keyboard: true }));
+    if(adminsList.includes(String(msg.from.id))){
+        bot.sendMessage(msg.from.id, "Технобал приветствует дресс код гостей. Каждому необходим бальный наряд, подходящий для рейва. Удобный низ и блистательный верх. Бальные платья, коктейльные платья, фраки и смокинги. Организаторы не ограничивают фантазию, но напоминают, что это все же бал.\n\nДля категории Debutant дресс-код обязателен.", admin_rk.open({ resize_keyboard: true }));
+    }
+    else{
+        rk = new ntkw_module.ReplyKeyboard();
+        rk
+        .addRow("ABOUT")
+        .addRow("DRESSCODE")
+        .addRow("LINEUP")
+        .addRow("ENTRANCE");
+        let info = "Технобал приветствует дресс код гостей. Каждому необходим бальный наряд, подходящий для рейва. Удобный низ и блистательный верх. Бальные платья, коктейльные платья, фраки и смокинги. Организаторы не ограничивают фантазию, но напоминают, что это все же бал.\n\nДля категории Debutant дресс-код обязателен.";
+        bot.sendMessage(msg.from.id, info, rk.open({ resize_keyboard: true }));
+    }
 });
 
 bot.onText(/\/how/i, (msg) => {
--- db_users.js
+++ db_users.js
@@ -1,13 +1,10 @@
 "use strict"
 
-// to know your password and uri: heroku config | grep MONGODB_URI
-
 let fs = require('fs-promise');
 let mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://dbuser1:dbuser1dbuser1@ds157422.mlab.com:57422/technoball_bot', {useNewUrlParser: true});
 
-// mongoose.connect('mongodb://localhost:27017/chamber', {useMongoClient: true});
 let  mongoosePaginate = require('mongoose-paginate');
 
 let userSchema = new mongoose.Schema({
@@ -28,24 +25,7 @@ function getByChatId(chat_id){
     .then (doc => {return Promise.resolve(doc)})
     .catch (err => {console.log(err); return Promise.reject(err)});
 }
-/*
-function getUserByFacebookIdAndName(id, token, username){
-    return Users.findOne({'facebook.username': username, 'facebook.id': id})
-    .then (doc => {
-            return Promise.resolve(doc);
-    })
-    .catch (err => {console.log(err); return Promise.reject(err)});
-}
 
-function checkNull (users){
-    return new Promise(function (resolve, reject) {
-        if (users.length !== 0){
-            reject("logintaken");
-        }
-        else resolve(users);
-    });
-}
-*/
 function add (user){
     return new Promise(function (resolve, reject) {
         if(user.answer.toLowerCase() === "arrectis")
@@ -69,180 +49,9 @@ function getAllPassed(){
     .catch(err => {return Promise.reject(err)});
 }
 
-/*
-function changePassword(user, pass){
-    return getUserByLoginAndPasshash(user.username, user.passhash)
-    .then(user => {
-        if (pass.newpassword0 === pass.newpassword1)
-        {
-            user.passhash = pass.newpassword0;
-            Users.update({'_id': user._id}, user, {upsert: false})
-            .then(data => {return Promise.resolve(user)})
-            .catch(err => {return Promise.reject("Can't update password due to .update() method error.")})
-            return Promise.resolve(user);
-        }
-        else
-            return Promise.reject("pass.oldpassword0 !== oldpassword1");
-    })
-    .catch(err => {return Promise.reject(err)});
-}
-
-function changeDescription(user, description){
-    return getUserByLoginAndPasshash(user.username, user.passhash)
-    .then(user => {
-        user.description = description;
-        Users.update({'_id': user._id}, user, {upsert: false})
-        .then(data => {return Promise.resolve(user)})
-        .catch(err => {
-            console.log(err); 
-            return Promise.reject(err)
-        })
-        return Promise.resolve(user)
-    })
-    .catch(err => {return Promise.reject(err)});
-}
-
-function addAvatar(req){
-    return getUserByLoginAndPasshash(req.user.username, req.user.passhash)
-    .then(user => {
-        console.log(req.files);
-        user.avatarUrl = req.files[0].url;
-        Users.update({'_id': user._id}, user, {upsert: false})
-        .then(data => {return Promise.resolve(user)})
-        .catch(err => {return Promise.reject("Can't update avatar due to .update() method error.\n Err: " + err)});
-        return Promise.resolve(user);
-    })
-    .catch(err => {return Promise.reject(err)});
-}
-
-function addTelegram(user, telegramUsername){
-    return getUserByLoginAndPasshash(user.username, user.passhash)
-    .then(user => {
-        user.telegramUsername = telegramUsername;
-        Users.update({'_id': user._id}, user, {upsert: false})
-        .then(data => {return Promise.resolve(user)})
-        .catch(err => {return Promise.reject("Can't add Telegram Username due to .update() method error.")})
-    })
-    .catch(err => {return Promise.reject(err)});
-}
-
-function addFacebook(user, facebook){
-    return new Promise(function (resolve, reject) {
-        Users.find( {'facebook.id': facebook.id})
-            .then (data => {
-                checkNull(data)
-                .then(() => {
-                    getUserByLoginAndPasshash(user.username, user.passhash)
-                    .then(user => {
-                        let updUser = user;
-                        updUser.facebook = {
-                            id: facebook.id,
-                            token: facebook.token,
-                            username: facebook.username
-                        };
-                
-                        Users.findOneAndUpdate({'_id': user._id}, updUser, {upsert:false}, function(err, doc){
-                            if (err) {
-                                console.log(err)
-                                return Promise.reject(err)
-                            }
-                            return Promise.resolve(updUser);
-                        });
-                        return Promise.resolve(updUser);
-                    })
-                    .catch(err => {return Promise.reject(err)});
-                })
-                .catch (err => reject(err));                 
-        })
-        .catch(err => reject(err));   
-	}); 
-}
-
-function addTwitter(user, twitterToken){
-    return getUserByLoginAndPasshash(user.username, user.passhash)
-    .then(user => {
-        user.twitterToken = twitterToken;
-        Users.update({'_id': user._id}, user, {upsert: false})
-        .then(data => {return Promise.resolve("Twitter added!")})
-        .catch(err => {return Promise.reject("Can't add Twitter due to .update() method error.")})
-    })
-    .catch(err => {return Promise.reject(err)});
-}
-
-let postsPerPage = 25;
-function getPostsByUser(user, page){
-    return new Promise((resolve, reject) => {
-        let audios = getPostsByCategory(user, "Audio");
-        let videos = getPostsByCategory(user, "Video");
-        let books  = getPostsByCategory(user, "Books");
-        let audiosPlusVideos = audios.concat(videos);
-        let allPosts = audiosPlusVideos.concat(books);
-        if (allPosts.length === 0)
-            return Promise.reject("User has no posts.")
-        // page counter starts from 1
-        let startIndex = (page - 1) * postsPerPage;
-        let endIndex = startIndex + postsPerPage;
-        let arrayToSend;
-        for (let i = startIndex, j = 0; i < endIndex; i++, j++)
-            arrayToSend[j] = allPosts[i];
-        return Promise.resolve(arrayToSend);
-    });
-}
-
-function getPostsByCategory(user, category){
-    return getUserByLoginAndPasshash(user.username, user.passhash)
-    .then(user => {
-        let posts;
-        if(category === "Video"){
-            posts = Video.getByAuthor(); 
-        }
-        else if(category === "Audio"){
-            posts = Audio.getByAuthor(); 
-        }
-        else if(category === "Books"){
-            posts = Books.getByAuthor(); 
-        }
-        else{
-            return Promise.reject("No such category.")
-        }
-        return Promise.resolve(posts);
-    })
-    .catch(err => {return Promise.reject(err)});
-}
-
-function getFollowers(user){
-    return getUserByLoginAndPasshash(user.username, user.passhash)
-    .then(user => {
-        return Promise.resolve(user.followers)
-    })
-    .catch(err => {return Promise.reject(err)});
-}
-
-function getFollowingUsers(user){
-    return getUserByLoginAndPasshash(user.username, user.passhash)
-    .then(user => {
-        return Promise.resolve(user.followed)
-    })
-    .catch(err => {return Promise.reject(err)});
-}
-*/
 module.exports = {
     add,
     getAll,
     getByChatId,
     getAllPassed
-    /*
-    addAvatar,
-    addTwitter,
-    addTelegram,
-    addFacebook,
-    getFollowers,
-    changePassword,
-    getPostsByUser,
-    getFollowingUsers,
-    changeDescription,
-    getPostsByCategory,
-    getUserByFacebookToken,
-    getUserByLoginAndPasshash,
-    getUserByFacebookIdAndName*/
 }
