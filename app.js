c5ff4af2d0e507ecb565c8e42e3230d96d3f985a
Miracle-Aligner <soldieroftheuniverse@gmail.com>
2019-03-19 01:39:14 +0200

    make it better

--- app.js
+++ app.js
@@ -1,19 +1,299 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
-const ntkw_module = require("node-telegram-keyboard-wrapper");
-const TelegramBot = require("node-telegram-bot-api");
+
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
 const Users = require("./db_users.js");
 
-const PASSWORD = 'arrectis';
+const adminsList = [
+    '157371788'
+];
 
-const token = "771801276:AAE7V59LdwVtBWHwwLy6SZtkD3NyysZEVX8";
+const bot = new Telegraf("769848173:AAHugU3o4R1Lv8Ow-vDWXl7ZfMokiPoIs9Y")
+const publishedModules = 3;
+const publishedLections = 16;
+bot.use(Telegraf.log())
+
+const modules = [
+    {
+        name: 'МОДУЛЬ 1',
+        imgLink: "https://static.tildacdn.com/tild3932-3366-4161-a565-646234653164/_1.png",
+        link: "http://znohistory.ed-era.com/m1", 
+        description: "Про розселення слов'ян, поділ земель, навалу монголів та появу перших козаків.", 
+        lections: [
+            { 
+                number: "Лекція 1",
+                fullname: "ВСТУП ДО ІСТОРІЇ УКРАЇНИ.",
+                description: "Історія про те, що таке історія взагалі та як нам вдається вивчати те, що вже минуло.",
+                link: "http://znohistory.ed-era.com/m1/l1",
+                imgLink: "https://static.tildacdn.com/tild6561-3463-4039-b234-313962653133/1.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 2",
+                fullname: "СТАРОДАВНЯ ІСТОРІЯ УКРАЇНИ.",
+                description: "Історія про те, як на українських землях з'явилися люди і вирішили тут лишитися.",
+                link: "http://znohistory.ed-era.com/m1/l2",
+                imgLink: "https://static.tildacdn.com/tild3864-3139-4363-b831-393537323937/1.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 3",
+                fullname: "КИЇВСЬКА ДЕРЖАВА.",
+                description: "Історія про те, як князі землю ділили, шлюби укладали та з кочовиками воювали.",
+                link: "http://znohistory.ed-era.com/m1/l3",
+                imgLink: "https://static.tildacdn.com/tild6431-6333-4138-a233-303431656536/photo.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 4",
+                fullname: "Галицько-Волинська держава. Монгольська навала.",
+                description: "Історія про те, як Данило Галицький коронувався, а монголи вторглися на Русь.",
+                link: "http://znohistory.ed-era.com/m1/l4",
+                imgLink: "https://static.tildacdn.com/tild3037-3837-4137-a436-663030633539/photo.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 5",
+                fullname: "Українські землі у другій половині XIV - першій половині XVI ст.",
+                description: "Історія про війни, королів, козаків та Україну в складі іноземних держав.",
+                link: "http://znohistory.ed-era.com/m1/l5",
+                imgLink: "https://static.tildacdn.com/tild6331-3635-4636-b132-623133333263/photo.png",
+                audioLink:""
+            }
+        ],
+        numberOfLections: 5,
+        numberOfPublished: 5
+    },
+    {
+        name: 'МОДУЛЬ 2',
+        imgLink: "https://static.tildacdn.com/tild3361-3964-4532-b365-313161396665/2.png", 
+        link: "http://znohistory.ed-era.com/m2", 
+        fullname: "Друга половина ХVІ ст. – XVIII ст.",
+        description: "Про утворення та занепад Речі Посполитої, війни, повстання, угоди та ліквідацію українського козацтва.", 
+        lections: [
+            { 
+                number: "Лекція 6",
+                fullname: "Українські землі у складі Речі Посполитої (друга половина XVI ст.)",
+                description: "Історія про утворення Речі Постополитої, заснування Січі, Реформацію та Контрреформацію.",
+                link: "http://znohistory.ed-era.com/m2/l6",
+                imgLink: "https://static.tildacdn.com/tild6136-3232-4661-b037-386363306463/6.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 7",
+                fullname: "Українські землі в складі Речі Посполитої (перша половина XVII ст.)",
+                description: "Історія про козацькі походи, повстання, церковне життя та культуру українських земель.",
+                link: "http://znohistory.ed-era.com/m2/l7",
+                imgLink: "https://static.tildacdn.com/tild3839-3433-4238-a138-663734313263/photo.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 8",
+                fullname: "Національно-визвольна війна під проводом Б. Хмельницького середини XVII ст.",
+                description: "Історія про початок війни за незалежність, битви, угоди та перемир'я.",
+                link: "http://znohistory.ed-era.com/m2/l8",
+                imgLink: "https://static.tildacdn.com/tild6234-3036-4135-b737-646636303837/photo.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 9",
+                fullname: "Українські землі наприкінці 50-их у 80-ті рр. XVII ст.",
+                description: "Історія про роздробленість, війни та гетьманів, що надто часто змінювалися.",
+                link: "http://znohistory.ed-era.com/m2/l9",
+                imgLink: "https://static.tildacdn.com/tild3263-3130-4231-a635-306439653430/photo.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 10",
+                fullname: "Українські землі наприкінці XVII - у першій половині XVIII століття.",
+                description: "Історія про союз зі шведами, перемогу малоросійства та першу Конституцію.",
+                link: "http://znohistory.ed-era.com/m2/l10",
+                imgLink: "https://static.tildacdn.com/tild6532-6636-4366-a233-343361373332/photo.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 11",
+                fullname: "Українські землі у другій половині XVIII ст.",
+                description: "Історія про реформи ліквідацію козацтва, масштабні повтання та три поділи Речі Постополитої.",
+                link: "http://znohistory.ed-era.com/m2/l11",
+                imgLink: "https://static.tildacdn.com/tild3838-3236-4137-a437-323664373832/12.png",
+                audioLink:""
+            }
+        ],
+        numberOfLections: 6,
+        numberOfPublished: 6
+    },
+    {
+        name: 'МОДУЛЬ 3',
+        fullname: "XIX століття.",
+        imgLink: "https://static.tildacdn.com/tild3364-6437-4164-b435-623331353435/Module_3.jpg",
+        description: "Про українські землі в складі Російської та Австрійської імперій, національне відродження та українську культуру", 
+        link: "http://znohistory.ed-era.com/m3",
+        lections: [
+            { 
+                number: "Лекція 12",
+                fullname: "Українські землі у складі Російської імперії наприкінці XVIII - у першій половині XIX ст.",
+                description: "Історія про територіальні зміни, українське національне відродження та масонів.",
+                link: "http://znohistory.ed-era.com/m3/l12",
+                imgLink: "https://static.tildacdn.com/tild3566-3762-4666-a132-353332656639/12.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 13-14",
+                fullname: "Західноукраїнські землі в складі Австрійської імперії наприкінці XVIII - у першій половині XIX ст., культура.",
+                description: "Історія про весну народів, будителів та Руську трійцю.",
+                link: "http://znohistory.ed-era.com/m3/l13-l14",
+                imgLink: "https://static.tildacdn.com/tild3737-3133-4133-a437-336665613930/_-1.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 15",
+                fullname: "Наддніпрянська Україна в другій половині XIX ст.",
+                description: "-",
+                link: "http://znohistory.ed-era.com/m3/l15",
+                imgLink: "https://static.tildacdn.com/tild6165-6536-4663-a339-653537636264/_15.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 16",
+                fullname: "Західноукраїнські землі у складі Австро-Угорської імперії в другій половині XIX ст.",
+                description: "-",
+                link: "http://znohistory.ed-era.com/m3/l16",
+                imgLink: "https://static.tildacdn.com/tild3131-3363-4361-a539-326662313462/photo.png",
+                audioLink:""
+            },
+            { 
+                number: "Лекція 17",
+                fullname: "Культура України в другій половині XIX - на початку XX ст.",
+                description: "-",
+                link: "http://znohistory.ed-era.com/m3/l17",
+                imgLink: "https://static.tildacdn.com/tild6434-6461-4265-b932-623234633761/Lecturre17.png",
+                audioLink:""
+            }
+        ],
+        numberOfLections: 5,
+        numberOfPublished: 2
+    }
+];
+
+
+    const modulesMenu = Telegraf.Extra
+                    .markdown()
+                    .markup((m) => m.keyboard([
+                    m.callbackButton(modules[0].name),
+                    m.callbackButton(modules[1].name),
+                    m.callbackButton(modules[2].name)
+                    ]).resize())
+
+    bot.command('start', ({ reply }) =>
+        reply('Виберіть модуль:', modulesMenu, Users.add({
+            chat_id: msg.from.id,
+            first_name: msg.from.first_name,
+            username: msg.from.username,
+            last_name: msg.from.last_name,
+            isAdmin: adminsList.includes(String(msg.from.id))
+        })
+    ))
+
+    bot.hears(/\МОДУЛЬ (\d+)/, (ctx) => {
+        if(ctx.match[1] < 1 || ctx.match[1] > publishedModules)
+            return ctx.reply("Такого модуля не існує або він ще не був опублікований 😔", modulesMenu);
+        else
+            return ctx.replyWithHTML(getModuleInfoHTML(ctx.match[1] - 1), getLectionsMenu(ctx.match[1]));
+    })
+
+    bot.hears(/\Лекція (\d+)/, (ctx) => {
+        let songsArr = [
+            "https://fex.net/load/888840093332/1309398491",
+            "https://fex.net/load/888840093332/1309398527",
+            "https://fex.net/load/888840093332/1309398576",
+            "https://fex.net/load/888840093332/1309398596",
+            "https://fex.net/load/888840093332/1309398622",
+            "https://fex.net/load/888840093332/1309398652",
+            "https://fex.net/load/888840093332/1309398709"
+        ];
+
+        if(ctx.match[1] < 1 || ctx.match[1] > publishedLections)
+            return ctx.reply("Такої лекції не існує або вона ще не була опублікована 😔", modulesMenu);
+        else
+            return ctx.replyWithHTML(getLectionInfoHTML(ctx.match[1]), ctx.replyWithAudio(songsArr[Math.floor(Math.random() * 6)]));
+    })
+
+    bot.hears('⬅️ Назад', (ctx) => {
+        ctx.reply('Виберіть модуль:', modulesMenu);
+    })
+
+    function getModuleInfoHTML(number){
+        return  "<b>" + modules[number].name + 
+                "</b><a href=\'" + modules[number].imgLink + 
+                "\'>.</a>\n\n<i>" + modules[number].description + 
+                "</i>\n\n<a href=\'" + modules[number].link + 
+                "\'>Переглянути лекції на сайті... </a>";
+    };
+
+    function getLectionInfoHTML(number){
+        let neededLection = getLection("Лекція " + number);
+
+        if (!neededLection)
+            return "Такої лекції не існує або вона ще не була опублікована 😔";
+        
+            return  "<b>" + neededLection.number + 
+                "</b><a style=\"text-decoration: none; color: black;\"href=\'" 
+                + neededLection.imgLink + 
+                "\'>: </a>" + neededLection.fullname +
+                "\n\n<i>" + neededLection.description + 
+                "</i>\n\n<a href=\'" + neededLection.link + 
+                "\'>Переглянути лекцію на сайті... </a>";
+    };
+
+    function getLection(name) {
+        let i = 0; // module
+        let j = 0;
+        while (i < modules.length && j < modules[i].numberOfLections){
+            if(modules[i].lections[j].number.includes(name))
+                return modules[i].lections[j];
+            j++;
+            if (j === modules[i].numberOfLections){
+                i++;
+                j = 0;
+            }  
+        }
+        return false;
+      }
+
+    function getLectionsMenu(moduleNumber){
+        let lectionsList = [];
+            modules[moduleNumber - 1].lections.forEach(element => {
+                lectionsList.push(element.number);
+            });
+            lectionsList.push("⬅️ Назад")
+
+            let lectionsMenu = Telegraf.Extra
+                .markup((m) => m.keyboard(lectionsList).resize());
+        console.log(lectionsMenu);
+        return lectionsMenu;
+        /*
+        return new Promise (function (resolve, reject) {
+            let lectionsMenu = [];
+            modules[moduleNumber].lections.forEach(element => {
+                lectionsMenu.push(element.number);
+            });
+            lectionsMenu.push("⬅️ Назад")
+            resolve (lectionsMenu)
+        });*/
+    }; 
+
+bot.launch()
+/*
 const bot = new TelegramBot(token, { polling: true });
 let rk = new ntkw_module.ReplyKeyboard();
 let admin_rk = new ntkw_module.ReplyKeyboard();
 const ik = new ntkw_module.InlineKeyboard();
 const commandsArray = [
-    'Добавить e-mail',
     'ABOUT',
     'ENTRANCE',
     'DRESSCODE',
@@ -44,30 +324,6 @@ admin_rk
 ik
     .addRow({ text: "✅ Опубликовать ", callback_data: "send" }, { text: "❌ Отменить ", callback_data: "decline" });
 
-bot.on("message", (msg) => {
-    console.log(msg);
-    let mes = "id: " + msg.from.id;
-    if (msg.from.username == undefined)
-        mes += "\nusername: -";
-    else{
-        mes += "\nusername: @" + msg.from.username;
-    }
-    if (msg.from.first_name == undefined)
-        mes += "\nfirst_name: -";
-    else{
-        mes += "\nfirst_name: " + msg.from.first_name;
-    }
-    if (msg.from.last_name == undefined)
-        mes += "\nlast_name: -";
-    else{
-        mes += "\nlast_name: " + msg.from.last_name;
-    }
-    mes += "\ntext: " + msg.text;
-
-    console.log(msg);
-
-    // bot.sendMessage(157371788, mes);
-});
 bot.onText(/\/start/i, (msg) => {
     if(adminsList.includes(String(msg.from.id))){
         bot.sendMessage(msg.from.id, "Добро пожаловать, " + msg.from.first_name + "!", admin_rk.open({ resize_keyboard: true }));
@@ -96,7 +352,7 @@ bot.onText(/\/start/i, (msg) => {
                     .addRow("DRESSCODE")
                     .addRow("LINEUP")
                     .addRow("ENTRANCE");
-                    bot.sendMessage(msg.from.id, "Поздравляю! Это правильное слово. Теперь ты в списке претендетов на получение пригласительного на Технобал.\nОжидай дальнейших инструкций здесь и у себя на почте.\n", rk.open({ resize_keyboard: true }));
+                    bot.sendMessage(msg.from.id, "Поздравляю! Это правильное слово, но на этом наша история не заканчивается...", rk.open({ resize_keyboard: true }));
                 }
                     
                 else 
@@ -141,7 +397,7 @@ const getInput = (msg) => {
                     .addRow("LINEUP")
                     .addRow("ENTRANCE");
                     bot.removeReplyListener(replyListenerId)
-                    bot.sendMessage(msg.from.id, "Поздравляю! Это правильное слово. Теперь ты в списке претендетов на получение пригласительного на Технобал.\nОжидай дальнейших инструкций здесь и у себя на почте.\n", rk.open({ resize_keyboard: true }));
+                    bot.sendMessage(msg.from.id, "Поздравляю! Это правильное слово, но на этом наша история не заканчивается...", rk.open({ resize_keyboard: true }));
                     reject(`done`);
                 } 
                 setTimeout(() => {
@@ -177,7 +433,7 @@ bot.onText(/Ввести кодовое слово/i, (msg) => {
                 .addRow("DRESSCODE")
                 .addRow("LINEUP")
                 .addRow("ENTRANCE"); 
-                bot.sendMessage(msg.from.id, "Поздравляю! Это правильное слово. Теперь ты в списке претендетов на получение пригласительного на Технобал.\nОжидай дальнейших инструкций здесь и у себя на почте.\n", rk.open({ resize_keyboard: true }));
+                bot.sendMessage(msg.from.id, "Поздравляю! Это правильное слово, но на этом наша история не заканчивается...", rk.open({ resize_keyboard: true }));
             }
                 
             else 
@@ -213,7 +469,7 @@ bot.onText(/\/code_word/i, (msg) => {
                 .addRow("DRESSCODE")
                 .addRow("LINEUP")
                 .addRow("ENTRANCE");
-                bot.sendMessage(msg.from.id, "Поздравляю! Это правильное слово. Теперь ты в списке претендетов на получение пригласительного на Технобал.\nОжидай дальнейших инструкций здесь и у себя на почте.\n", rk.open({ resize_keyboard: true }));
+                bot.sendMessage(msg.from.id, "Поздравляю! Это правильное слово, но на этом наша история не заканчивается...", rk.open({ resize_keyboard: true }));
             }
                 
             else 
@@ -435,3 +691,5 @@ bot.on("callback_query", (query) => {
 
 
 bot.on("polling_error", (err) => console.log(err));
+
+*/
--- db_users.js
+++ db_users.js
@@ -3,7 +3,7 @@
 let fs = require('fs-promise');
 let mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://dbuser1:dbuser1dbuser1@ds157422.mlab.com:57422/technoball_bot', {useNewUrlParser: true});
+mongoose.connect('mongodb://dbuser1:dbuser1dbuser1@ds163825.mlab.com:63825/znohistory_bot', {useNewUrlParser: true});
 
 let  mongoosePaginate = require('mongoose-paginate');
 
@@ -16,6 +16,7 @@ let userSchema = new mongoose.Schema({
     mail: String,
     answer: String,
 }, {collection: "Users"});
+
 userSchema.plugin(mongoosePaginate);
 let db = mongoose.connection;
 let Users = mongoose.model('Users', userSchema, 'Users');
@@ -28,8 +29,6 @@ function getByChatId(chat_id){
 
 function add (user){
     return new Promise(function (resolve, reject) {
-        if(user.answer.toLowerCase() === "arrectis")
-            user.answer = "arrectis";
         db.collection('Users').insert(user, (err, data) => {
             if (err) reject("cannnot insert into db");
             else resolve(user);
@@ -43,15 +42,9 @@ function getAll(){
     .catch(err => {return Promise.reject(err)});
 }
 
-function getAllPassed(){
-    return Users.find({"answer":"arrectis"})
-    .then(doc => {return Promise.resolve(doc)})
-    .catch(err => {return Promise.reject(err)});
-}
 
 module.exports = {
     add,
     getAll,
-    getByChatId,
-    getAllPassed
+    getByChatId
 }
