7f698f8db76ce4c68b85c2789351b31f5cc59996
Miracle-Aligner <soldieroftheuniverse@gmail.com>
2019-03-19 18:35:47 +0200

    make it better

--- app.js
+++ app.js
@@ -206,7 +206,7 @@ const modules = [
             chat_id: msg.from.id,
             message: msg.text
         });
-        console.log("HEEEEEEEEEEEEEEEEYY" + msg);
+        console.log("HEEEEEEEEEEEEEEEEYY" + string(msg));
     })
 
     bot.hears(/\МОДУЛЬ (\d+)/, (ctx) => {
